502af7ed8b7e27ea08fb2a73b8856e70ce73c7e2
DanielKH1862 <145186826+DanielKH1862@users.noreply.github.com>
2024-10-22 17:58:19 +0700

    Delete routes directory

--- routes/MhsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const MhsController = require('../controllers/MhsController');
-
-router.get('/mahasiswa', MhsController.getMahasiswa);
-
-module.exports = router;
