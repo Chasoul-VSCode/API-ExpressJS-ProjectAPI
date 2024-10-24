3c669fc09900da32111c94c960c9ca69dc787db8
DanielKH1862 <145186826+DanielKH1862@users.noreply.github.com>
2024-10-24 13:55:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const kurirRoutes = require('./Routes/connRoutes');
+const kurirRoutes = require('./Routes/ConnRoutes');
 
 const app = express();
 const port = 3000;
